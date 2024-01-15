a06cd6ae2f58f6396351bf8c7a5e105f986b6137
kleberson154 <kleberson55@hotmail.com>
2024-01-15 16:26:50 -0300

    Adding Result and Summary to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import Result from './components/Result'
+import Summary from './components/Summary'
+
 function App() {
   return (
     <>
       <div className="h-screen w-screen flex justify-center items-center">
-        <div className="bg-pale">hello</div>
+        <div className="bg-white rounded-3xl flex shadow-2xl">
+          <Result />
+          <Summary />
+        </div>
       </div>
     </>
   )
